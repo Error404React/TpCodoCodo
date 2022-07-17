86e6c75fb65c275c3279232fe41e1a9f4349c49b
Nicole Perez <niqui.steffi@hotmail.com>
2022-07-17 11:55:57 -0300

    moficicaciones en footer

--- TP/tp-codo-a-codo/src/components/PhotoPlaceholder.jsx
+++ TP/tp-codo-a-codo/src/components/PhotoPlaceholder.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../styles/PhotoPlaceholder.css'
 
 function PhotoPlaceholder(){
     return (
       <>
-        <a href="https://placeholder.com">
+        <a className='aImage' href="https://placeholder.com">
           <img className='newsImage' src="https://via.placeholder.com/280x150.png?text=Noticia+sin+imágen" alt=''/>
         </a>
       </>
